61caee09e519df5681d854088f0bea58bc9f0e8d
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-20 17:54:29 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -2,7 +2,8 @@
 
 BreachType inferBreach(BatteryCharacter batteryCharacteristics, float tempValue);
 BatteryCharacter populateOperatingTemperatureValues(CoolingType coolingType);
-void checkAndAlert(AlertTarget alertTarget, CoolingType coolingType, float tempValue);
+bool checkAndAlert(AlertTarget alertTarget, CoolingType coolingType, float tempValue);
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 void alertBreach(AlertTarget alertTarget, BreachType breachType);
+bool validateEnumValue(int enumValue, int maxPossibleValue);
