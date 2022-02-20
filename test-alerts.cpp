83eb7e2b4aacd6ccc36adcb5a79f5f48001760ab
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-20 14:51:42 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "alertConfigParameters.h"
 #include "typewise-alert.h"
 
 TEST_CASE("InferBreach -Temperature value is less than operating lower limit") {
