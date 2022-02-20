07b8b5d0e23737f99b5d60795e3fbf483a9edade
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-20 14:39:20 +0530

    Update alertConfigParameters.h

--- alertConfigParameters.h
+++ alertConfigParameters.h
@@ -1,1 +1,27 @@
+
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+typedef struct {
+  CoolingType coolingType;
+  float lowerLimitOfTemp;
+  float upperLimitOfTemp;
+  char brand[48];
+} BatteryCharacter;
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
 extern BatteryCharacter BatteryTemperatureValues[3];
+extern char AlertMessageOverEmail[MAX_BREACH_TYPES][100];
