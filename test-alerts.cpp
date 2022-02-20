4afcec1e08dbe023bbcdbb93066f583f041f4fc9
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-20 14:21:18 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,10 +3,6 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-}
-
 TEST_CASE("InferBreach -Temperature value is less than operating lower limit") {
   BatteryCharacter batteryCharacteristics;
   batteryCharacteristics.lowerLimitOfTemp  = 20;
